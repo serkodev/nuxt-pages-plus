9dbe5c656c08cccc3c49617c244d7578ec8554d9
SerKo <serko.dev@gmail.com>
2024-04-08 18:21:41 +0800

    layout: layout boundary

--- playground/layouts/default.vue
+++ playground/layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="h-screen grid grid-cols-[400px_1fr_400px] gap-px">
-    <NamedPage name="left" />
-    <slot />
-    <NamedPage name="right" />
-  </div>
+  <TheBoundary label="Layout: default" color="gray" class="h-screen flex p-6">
+    <div class="flex-1 grid grid-cols-[400px_1fr_400px] gap-px">
+      <NamedPage name="left" />
+      <slot />
+      <NamedPage name="right" />
+    </div>
+  </TheBoundary>
 </template>
