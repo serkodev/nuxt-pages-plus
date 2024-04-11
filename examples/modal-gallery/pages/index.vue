cc743bd7b96f93c581584e4545949b42093fc360
SerKo <serko.dev@gmail.com>
2024-04-12 06:23:05 +0800

    chore(example): update

--- examples/modal-gallery/pages/index.vue
+++ examples/modal-gallery/pages/index.vue
@@ -1,18 +1,18 @@
-<script setup lang="ts">
-const { push } = useModalRouter()
-</script>
-
 <template>
   <TheBoundary label="$__PAGES_PATH__" class="h-screen flex flex-col gap-6 items-center justify-center">
     <div class="grid grid-cols-3 gap-6">
-      <button v-for="i in 9" :key="i" class="size-32 items-center justify-center text-4xl bg-green-400 text-black rounded-xl" @click="push(`/gallery/${i}`)">
-        {{ i }}
-      </button>
+      <PlusModalLink v-for="i in 9" :key="i" :to="`/gallery/${i}`">
+        <button class="size-32 items-center justify-center text-4xl bg-green-400 text-black rounded-xl">
+          {{ i }}
+        </button>
+      </PlusModalLink>
     </div>
 
-    <UButton class="!bg-yellow-500" @click="push('/info/1')">
-      Open Info
-    </UButton>
+    <PlusModalLink to="/info/1">
+      <UButton class="!bg-yellow-500">
+        Open Info
+      </UButton>
+    </PlusModalLink>
 
     <PlusModalPage name="modal" />
   </TheBoundary>
