3733717b59bd089637f89d39aee0641653610097
SerKo <serko.dev@gmail.com>
2024-04-12 01:30:42 +0800

    feat: add ModalPage helper component

--- examples/modal-gallery/pages/index.vue
+++ examples/modal-gallery/pages/index.vue
@@ -14,6 +14,6 @@ const { push } = useModalRouter()
       Open Index Modal
     </UButton>
 
-    <ParallelPage name="modal" />
+    <ModalPage name="modal" />
   </TheBoundary>
 </template>
---
+++ src/runtime/components/ModalPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  // Unique name of the parallel router
+  name: string
+}>()
+</script>
+
+<template>
+  <ParallelPage :name auto-hide v-bind="$attrs" />
+</template>
