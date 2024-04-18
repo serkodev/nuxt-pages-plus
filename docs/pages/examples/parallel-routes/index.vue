008438b0e55a1ef2e5a6a189e7e41c41ee748fb8
SerKo <serko.dev@gmail.com>
2024-04-18 20:09:33 +0800

    docs: fix parallel routes example for SSG

--- docs/pages/examples/parallel-routes/index.vue
+++ docs/pages/examples/parallel-routes/index.vue
@@ -1,7 +1,19 @@
 <template>
   <TheBoundary label="$__PAGES_PATH__">
-    <div class="flex p-6">
-      <ContentDoc />
+    <div class="p-6">
+      <h1 class="font-semibold mb-4">
+        Home
+      </h1>
+
+      <p class="my-2">
+        With parallel routes, you can render the left side menu and change the URL.
+      </p>
+      <p class="my-2">
+        When you click <code class="text-nuxt">Hot</code> category, the URL changes to <code class="text-nuxt">/hot</code> and the left side menu changes to hot news.
+      </p>
+      <p class="my-2">
+        If you click any news topic on the left side menu, the URL will changes but the left side menu will remain the same.
+      </p>
     </div>
   </TheBoundary>
 </template>
