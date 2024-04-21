eabc5fcee7ce7523a8b380c30e88469e62dbaa2b
SerKo <serko.dev@gmail.com>
2024-04-21 13:43:40 +0800

    docs(example): update docs for index fallback

--- docs/layouts/parallel.vue
+++ docs/layouts/parallel.vue
@@ -29,25 +29,7 @@ const relativePath = computed(() => {
             </NuxtLink>
           </div>
 
-          <PlusParallelPage name="left">
-            <template #index>
-              <TheBoundary label="layouts/parallel.vue > PlusParallelPage #index" class="p-4">
-                <p class="mb-4">
-                  This is the index view of the @left parallel routes.
-                </p>
-
-                <p class="my-4">
-                  The reason of rendering this view because when the page is init loaded but cannot find the match routes for <code class="text-nuxt">{{ relativePath }}</code>.
-                </p>
-
-                <p>
-                  Instead of render this <code>#index</code> slot, you can set the <NuxtLink to="/getting-started/configuration#index">
-                    <code class="text-nuxt">index</code>
-                  </NuxtLink> in config, so it will redirect to the index page of the parallel routes.
-                </p>
-              </TheBoundary>
-            </template>
-          </PlusParallelPage>
+          <PlusParallelPage name="left" />
         </div>
         <slot />
       </div>
---
+++ docs/pages/examples/parallel-routes/@left/~index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const relativePath = computed(() => {
+  return useRoute().path.replace(/^\/examples\/parallel-routes/, '') || '/'
+})
+</script>
+
+<template>
+  <TheBoundary label="$__PAGES_PATH__" class="p-4">
+    <p class="mb-4">
+      This is the index view of the @left parallel routes.
+    </p>
+
+    <p class="my-4">
+      The reason of rendering this view because when the page is init loaded but cannot find the match routes for <code class="text-nuxt">{{ relativePath }}</code>.
+    </p>
+
+    <p>
+      Instead of render this <code class="text-nuxt">/~index</code> path, you can set the <NuxtLink to="/getting-started/configuration#index">
+        <code class="text-nuxt underline">index</code>
+      </NuxtLink> in config, so it will redirect to the index page of the parallel routes.
+    </p>
+  </TheBoundary>
+</template>
