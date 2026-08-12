15fb3cc45a353d567554fc60e02b789ec2e3ef30
SerKo <serko.dev@gmail.com>
2026-08-12 22:58:48 +0800

    refactor(examples): drop `@faker-js/faker` dep, hard-code example news data

---
+++ examples/base/utils/news.ts
@@ -0,0 +1,63 @@
+// static demo data (originally generated with faker, seed 123)
+export const exampleNews = [
+  {
+    title: 'Circumvenio calamitas ipsa super curo vomer stillicidium denique.',
+    content: 'Conicio suspendo decens adulatio cubicularis taceo. Atque exercitationem exercitationem reiciendis. Surculus perferendis suppono commodi conturbo calco claudeo quos aliquam.\nCurvo depulso cursim color cursim varietas. Desipio quia ancilla comes cuppedia usitas casso denuncio voro earum. Animi trepide pax impedit conicio cognomen cur.\nUtique dolore sortitus occaecati quibusdam spero turbo agnosco. Careo autem mollitia allatus vae quis supra acceptus paens. Articulus argumentum sub comis stultus iste creta.',
+    author: 'Tiana',
+    ago: '20 hours ago',
+  },
+  {
+    title: 'Adimpleo cogo cubicularis sufficio vulnero.',
+    content: 'Tempus optio stultus arcus cubitum. Coniecto dolores sophismata amiculum. Comminor solvo tyrannus ipsum.\nCreo comes considero assumenda tricesimus confido ipsam nesciunt enim. Voveo ventosus beatus. Eius veniam vorago causa magni.\nCruentus suus arto paulatim uredo vorago ager cursus bardus. Infit aliquid clibanus vestigium modi defluo. Talis administratio suggero tumultus asperiores terminatio cito cohibeo sono.',
+    author: 'Berry',
+    ago: '16 hours ago',
+  },
+  {
+    title: 'Subito decerno debitis teneo maxime ago nostrum tot conduco vestigium.',
+    content: 'Natus tantillus ager unde trado verbera aperiam aggredior aptus. Curriculum libero annus baiulus tondeo delibero. Absens ipsam vicissitudo non basium supellex cras sordeo adamo.\nAdduco tamdiu demergo animus illo aeger solitudo vulnus. Natus ambulo subseco solvo admiratio textor earum cursim tertius. Denique attonbitus commemoro tutis audentia cur.\nCapillus veritatis versus aliqua delectus desipio combibo adiuvo carbo allatus. Timor vehemens adimpleo cogito. Fugit quidem abscido deorsum voveo corrupti alo deinde vix congregatio.',
+    author: 'Pink',
+    ago: '19 hours ago',
+  },
+  {
+    title: 'Decipio sumo cunctatio aufero.',
+    content: 'Soleo uredo acies cenaculum desipio aegre vulgo capillus corrumpo benigne. Candidus coerceo reiciendis. Conturbo abscido copiose explicabo arx.\nClarus ratione acquiro valens acceptus antepono tergiversatio. Sum volup ut. Vitae curtus uterque constans vetus arcesso vilicus tristis.\nAnser patior acceptus supplanto absens ago caelum utique convoco. Minus caelum mollitia solvo coadunatio curatio defessus. Ocer vinum iure desino abduco denique vestigium avarus admoveo cum.',
+    author: 'Gage',
+    ago: '20 hours ago',
+  },
+  {
+    title: 'Verumtamen stabilis ventito.',
+    content: 'Tonsor condico consectetur cribro temeritas corporis carbo. Ver conscendo rem certus basium conduco compello vaco traho sui. Curo caries angulus coerceo arbustum aliquam pax convoco maiores aureus.\nBibo chirographum talis laborum conculco illo suasoria verecundia. Candidus tamen tergiversatio averto tracto delego tergum. Concedo viriliter solus terebro.\nBardus deludo tolero spero absconditus alias conor vinco. Centum contra bene curiositas blandior tutis. Cibus vicissitudo comburo verbera adimpleo suffragium.',
+    author: 'Jazlyn',
+    ago: '11 hours ago',
+  },
+  {
+    title: 'Adhaero concedo vinitor.',
+    content: 'Corporis perspiciatis basium asper conturbo urbanus dolor. Virga totam commodi voluptas votum. Sollicito cultura causa agnitio celer cernuus.\nAudacia viriliter ambulo quibusdam decimus curriculum. Ustilo cicuta accusator versus urbs. Eos amitto aliquid tui cunae.\nVilla caries abutor acidus sui vesper deleo corrupti. Unde solio candidus teres appono asperiores placeat. Sufficio consectetur chirographum vulticulus.',
+    author: 'Alize',
+    ago: '15 hours ago',
+  },
+  {
+    title: 'Temporibus calculus alii.',
+    content: 'Synagoga spiritus admoneo claustrum defendo cito tollo aperte. Voveo veniam brevis a vomer vaco verto. Tam benevolentia cruciamentum ullus antiquus varietas.\nCursim cohaero veritatis eaque thymum umquam. Cognomen conforto pariatur decet vicissitudo cubicularis demulceo provident cultellus vulgivagus. Bos commodi arceo.\nTergum eos ademptio vomito praesentium. Solvo crapula appositus. Suscipio speciosus carmen esse fugiat sunt contigo.',
+    author: 'Pierce',
+    ago: '15 hours ago',
+  },
+  {
+    title: 'In unus minus.',
+    content: 'Dolore templum amo totidem. Explicabo creptio carpo sit. Temporibus et utique.\nAdeo arbitro theatrum deprimo decimus comis cinis. Damnatio vado socius una ulterius viscus sublime tibi tabella pecco. Sumptus adhaero ducimus textor carcer delego dapifer culpa.\nEst decumbo somnus. Acsi addo substantia suffragium vitiosus utor delibero. Defluo cado cotidie amet sophismata aureus demo volaticus.',
+    author: 'Alexandria',
+    ago: '4 hours ago',
+  },
+  {
+    title: 'Vilis ver arx vomica tandem.',
+    content: 'Vicinus utrum crinis sollicito sit compello attero. Celer conspergo virtus degusto stillicidium condico. Soluta autus altus villa vinco quas acceptus caelum thesis utique.\nCopiose certus angelus ancilla viridis titulus. Beatae sollers tendo tollo. Vorax calcar odit odio.\nSolus numquam earum tendo amiculum abbas viridis deserunt compono corona. Creber tepesco decet tutamen aestus denique deleo celo villa. Decor alter beatae cernuus denuo confido teres demonstro usus vulnero.',
+    author: 'Daija',
+    ago: '15 hours ago',
+  },
+  {
+    title: 'Vinculum clam suscipio accendo utroque adversus tabella vulgus desino.',
+    content: 'Pariatur quia sordeo at. Quas adimpleo stella autus acsi ipsa torqueo undique amet somnus. Claustrum volup cibo aegrus earum suasoria caries confido.\nValde tametsi bellum caste error tenus quaerat. Patria temperantia fuga velit vinitor veritas tego. Creo cibus solus commodo.\nAmplitudo terga occaecati truculenter curvo quibusdam iste voluptates temeritas. Atavus ventito basium sol video caecus caelum. Tres consequuntur celo antiquus votum truculenter velum doloribus amplus.',
+    author: 'Amie',
+    ago: '8 hours ago',
+  },
+]
